27955d3a41816ecfdbf648803750b553d9345ed5
HamzaSafdar26 <hamzasafdar260@gmail.com>
2016-12-19 21:35:49 +0500

    Add files via upload

---
+++ FA13BSE145.java
@@ -0,0 +1,10 @@
+package hamza;
+
+public class FA13BSE145 {
+	
+	public void run() {
+
+		System.out.println("Method in Class FA13-BSE-145");
+	}
+
+}
---
+++ Main.java
@@ -0,0 +1,15 @@
+package hamza;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		FA13BSE120 A = new FA13BSE120();
+		A.run();
+		FA13BSE145 B = new FA13BSE145();
+		B.run();
+
+	}
+
+}
